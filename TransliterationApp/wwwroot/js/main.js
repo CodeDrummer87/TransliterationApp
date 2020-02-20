3d182f0d0ba991496abb91ba8cde774dc83fa061
CodeDrummer <code.drummer87@gmail.com>
2020-02-21 03:35:07 +0600

    'SaveSourceText' pop-up created

--- TransliterationApp/wwwroot/js/main.js
+++ TransliterationApp/wwwroot/js/main.js
@@ -1,4 +1,6 @@
-﻿$(document).ready(function () {
+﻿var currentPopup = null;
+
+$(document).ready(function () {
     var windowHeight = window.innerHeight;
 
     let headerHeight = windowHeight / 9;
@@ -7,4 +9,27 @@
     document.querySelector('footer').style.height = footerHeight + "px";
     let mainHeight = windowHeight - headerHeight - footerHeight - 50;
     document.querySelector('main').style.height = mainHeight + "px";
+
+    $('body').on('click', '.nap', function () {
+        HideNap();
+    });
+
+    $('.pop-up-cancel').on('click', 'span', function () {
+        HideNap();
+    });
+
+    $('.left-block').on('click', '#saveSourceText', function () {
+        ShowNap('.pop-up-saveSourceText');
+    });
 });
+
+function ShowNap(modal) {
+    currentPopup = modal;
+    $('.nap').css('display', 'block');
+    $(modal).css('display', 'block');
+}
+
+function HideNap() {
+    $(currentPopup).css('display', 'none');
+    $('.nap').css('display', 'none');
+}
