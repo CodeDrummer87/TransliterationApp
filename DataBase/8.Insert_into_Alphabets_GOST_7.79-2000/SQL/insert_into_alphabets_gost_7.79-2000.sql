576f90850569f0694355cca9a379b8ba026b2af3
CodeDrummer <code.drummer87@gmail.com>
2020-03-01 16:53:07 +0600

    'Alphabets' model is modified. First transliteration system 'GOST 7.79-2000' is inserted

---
+++ DataBase/8.Insert_into_Alphabets_GOST_7.79-2000/SQL/insert_into_alphabets_gost_7.79-2000.sql
@@ -0,0 +1,42 @@
+IF NOT EXISTS (SELECT * FROM dbo.sysobjects WHERE ID = OBJECT_ID(N'TransliterationSystem'))
+USE TransliterationSystem;
+
+GO
+
+IF OBJECT_ID('Alphabets', 'u') IS NOT NULL
+INSERT INTO Alphabets VALUES (
+	'GOST 7.79-2000',
+	'A', 'a',
+	'B', 'b',
+	'V', 'v',
+	'G', 'g',
+	'D', 'd',
+	'E', 'e',
+	'YO', 'yo',
+	'ZH', 'zh',
+	'Z', 'z',
+	'I', 'i',
+	'J', 'j',
+	'K', 'k',
+	'L', 'l',
+	'M', 'm',
+	'N', 'n',
+	'O', 'o',
+	'P', 'p',
+	'R', 'r',
+	'S', 's',
+	'T', 't',
+	'U', 'u',
+	'F', 'f',
+	'KH', 'kh',
+	'TS', 'ts',
+	'CH', 'ch',
+	'SH', 'sh',
+	'SHH', 'shh',
+	'&#x22',
+	'Y', 'y',
+	'&#x27',
+	'EH', 'eh',
+	'YU', 'yu',
+	'YA', 'ya'
+);
--- TransliterationApp/Models/Alphabet.cs
+++ TransliterationApp/Models/Alphabet.cs
@@ -11,38 +11,69 @@ namespace TransliterationApp.Models
         [Key]
         public int SystemId { get; set; }
         public string SystemName { get; set; }
-        public string RUS_A { get; set; }
-        public string RUS_B { get; set; }
-        public string RUS_V { get; set; }
-        public string RUS_G { get; set; }
-        public string RUS_D { get; set; }
-        public string RUS_E { get; set; }
-        public string RUS_YO { get; set; }
-        public string RUS_ZH { get; set; }
-        public string RUS_Z { get; set; }
-        public string RUS_I { get; set; }
-        public string RUS_J { get; set; }
-        public string RUS_K { get; set; }
-        public string RUS_L { get; set; }
-        public string RUS_M { get; set; }
-        public string RUS_N { get; set; }
-        public string RUS_O { get; set; }
-        public string RUS_P { get; set; }
-        public string RUS_R { get; set; }
-        public string RUS_S { get; set; }
-        public string RUS_T { get; set; }
-        public string RUS_U { get; set; }
-        public string RUS_F { get; set; }
-        public string RUS_KH { get; set; }
-        public string RUS_C { get; set; }
-        public string RUS_CH { get; set; }
-        public string RUS_SH { get; set; }
-        public string RUS_SHCH { get; set; }
+        public string RUS_UPPERCASE_A { get; set; }
+        public string RUS_a { get; set; }
+        public string RUS_UPPERCASE_B { get; set; }
+        public string RUS_b { get; set; }
+        public string RUS_UPPERCASE_V { get; set; }
+        public string RUS_v { get; set; }
+        public string RUS_UPPERCASE_G { get; set; }
+        public string RUS_g { get; set; }
+        public string RUS_UPPERCASE_D { get; set; }
+        public string RUS_d { get; set; }
+        public string RUS_UPPERCASE_E { get; set; }
+        public string RUS_e { get; set; }
+        public string RUS_UPPERCASE_YO { get; set; }
+        public string RUS_yo { get; set; }
+        public string RUS_UPPERCASE_ZH { get; set; }
+        public string RUS_zh { get; set; }
+        public string RUS_UPPERCASE_Z { get; set; }
+        public string RUS_z { get; set; }
+        public string RUS_UPPERCASE_I { get; set; }
+        public string RUS_i { get; set; }
+        public string RUS_UPPERCASE_J { get; set; }
+        public string RUS_j { get; set; }
+        public string RUS_UPPERCASE_K { get; set; }
+        public string RUS_k { get; set; }
+        public string RUS_UPPERCASE_L { get; set; }
+        public string RUS_l { get; set; }
+        public string RUS_UPPERCASE_M { get; set; }
+        public string RUS_m { get; set; }
+        public string RUS_UPPERCASE_N { get; set; }
+        public string RUS_n{ get; set; }
+        public string RUS_UPPERCASE_O { get; set; }
+        public string RUS_o { get; set; }
+        public string RUS_UPPERCASE_P { get; set; }
+        public string RUS_p { get; set; }
+        public string RUS_UPPERCASE_R { get; set; }
+        public string RUS_r { get; set; }
+        public string RUS_UPPERCASE_S { get; set; }
+        public string RUS_s { get; set; }
+        public string RUS_UPPERCASE_T { get; set; }
+        public string RUS_t { get; set; }
+        public string RUS_UPPERCASE_U { get; set; }
+        public string RUS_u { get; set; }
+        public string RUS_UPPERCASE_F { get; set; }
+        public string RUS_f { get; set; }
+        public string RUS_UPPERCASE_KH { get; set; }
+        public string RUS_kh { get; set; }
+        public string RUS_UPPERCASE_C { get; set; }
+        public string RUS_c { get; set; }
+        public string RUS_UPPERCASE_CH { get; set; }
+        public string RUS_ch { get; set; }
+        public string RUS_UPPERCASE_SH { get; set; }
+        public string RUS_sh { get; set; }
+        public string RUS_UPPERCASE_SHCH { get; set; }
+        public string RUS_shch{ get; set; }
         public string RUS_SolidSign { get; set; }
-        public string RUS_Y { get; set; }
+        public string RUS_UPPERCASE_Y { get; set; }
+        public string RUS_y { get; set; }
         public string RUS_SoftSign { get; set; }
-        public string RUS_EH { get; set; }
-        public string RUS_YU { get; set; }
-        public string RUS_YA { get; set; }
+        public string RUS_UPPERCASE_EH { get; set; }
+        public string RUS_eh { get; set; }
+        public string RUS_UPPERCASE_YU { get; set; }
+        public string RUS_yu { get; set; }
+        public string RUS_UPPERCASE_YA { get; set; }
+        public string RUS_ya { get; set; }
     }
 }
